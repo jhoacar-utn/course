7d8782d992d12419b930898783f1b70435a3c2b9
jhoacar <carrerojhoan@gmail.com>
2022-04-03 19:55:11 -0300

    fixed sed to root directory

--- create-branch.sh
+++ create-branch.sh
@@ -2,9 +2,10 @@ STUDENT=user_test;
 EXISTING=jhoan_carrero; 
 git checkout $EXISTING; 
 git checkout -b $STUDENT; 
-find . -iname "$EXISTING" | while read FILE; 
+find ./utn -iname "$EXISTING" | while read FILE; 
     do 
     NEW_FILE="$(sed -e "s/$EXISTING/$STUDENT/" <<< $FILE)"; 
     mv "${FILE}" "${NEW_FILE}" ; 
     done; 
-find . -type f -exec sed -i "s/$EXISTING/$STUDENT/g" {} \;
+find ./utn -type f -exec sed -i "s/$EXISTING/$STUDENT/g" {} \;
+find . -iname "*README*" -exec sed -i "s/$EXISTING/$STUDENT/g" {} \;
