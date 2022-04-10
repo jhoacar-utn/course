b1bfc60f63ed755743fa5d5ef21a98bef8b39aa9
jhoacar <carrerojhoan@gmail.com>
2022-04-10 13:07:11 -0300

    added files for database in each student

--- mongo/init/mongo_course-init.js
+++ mongo/init/mongo_course-init.js
@@ -1,7 +1,35 @@
 
 const users = [
 
+"agustin_ekkert_pazo",
+
+"cynthia_altmark",
+
+"evelyn_castillo",
+
+"federico_arabena",
+
 "jhoan_carrero",
+
+"jose_carballo",
+
+"lautaro_salazar_de_moya",
+
+"liam_marin",
+
+"mario_martinez_galarza",
+
+"matias_eckhardt",
+
+"maximiliano_vitti",
+
+"nicolas_maurizi",
+
+"pablo_defelitto",
+
+"santiago_franchi",
+
+"veronica_ferreyra",
 
 ];
 
---
+++ mysql/init/mysql_course-init.sql
@@ -0,0 +1,91 @@
+
+CREATE DATABASE IF NOT EXISTS agustin_ekkert_pazo;
+CREATE TABLE IF NOT EXISTS agustin_ekkert_pazo.welcome (message VARCHAR(100));
+INSERT INTO agustin_ekkert_pazo.welcome (message) VALUES ('Welcome to your database agustin ekkert pazo'); 
+CREATE USER agustin_ekkert_pazo@'%' IDENTIFIED BY 'agustin_ekkert_pazo';
+GRANT ALL ON agustin_ekkert_pazo.* TO agustin_ekkert_pazo@'%';
+        
+CREATE DATABASE IF NOT EXISTS cynthia_altmark;
+CREATE TABLE IF NOT EXISTS cynthia_altmark.welcome (message VARCHAR(100));
+INSERT INTO cynthia_altmark.welcome (message) VALUES ('Welcome to your database cynthia altmark'); 
+CREATE USER cynthia_altmark@'%' IDENTIFIED BY 'cynthia_altmark';
+GRANT ALL ON cynthia_altmark.* TO cynthia_altmark@'%';
+        
+CREATE DATABASE IF NOT EXISTS evelyn_castillo;
+CREATE TABLE IF NOT EXISTS evelyn_castillo.welcome (message VARCHAR(100));
+INSERT INTO evelyn_castillo.welcome (message) VALUES ('Welcome to your database evelyn castillo'); 
+CREATE USER evelyn_castillo@'%' IDENTIFIED BY 'evelyn_castillo';
+GRANT ALL ON evelyn_castillo.* TO evelyn_castillo@'%';
+        
+CREATE DATABASE IF NOT EXISTS federico_arabena;
+CREATE TABLE IF NOT EXISTS federico_arabena.welcome (message VARCHAR(100));
+INSERT INTO federico_arabena.welcome (message) VALUES ('Welcome to your database federico arabena'); 
+CREATE USER federico_arabena@'%' IDENTIFIED BY 'federico_arabena';
+GRANT ALL ON federico_arabena.* TO federico_arabena@'%';
+        
+CREATE DATABASE IF NOT EXISTS jhoan_carrero;
+CREATE TABLE IF NOT EXISTS jhoan_carrero.welcome (message VARCHAR(100));
+INSERT INTO jhoan_carrero.welcome (message) VALUES ('Welcome to your database jhoan carrero'); 
+CREATE USER jhoan_carrero@'%' IDENTIFIED BY 'jhoan_carrero';
+GRANT ALL ON jhoan_carrero.* TO jhoan_carrero@'%';
+        
+CREATE DATABASE IF NOT EXISTS jose_carballo;
+CREATE TABLE IF NOT EXISTS jose_carballo.welcome (message VARCHAR(100));
+INSERT INTO jose_carballo.welcome (message) VALUES ('Welcome to your database jose carballo'); 
+CREATE USER jose_carballo@'%' IDENTIFIED BY 'jose_carballo';
+GRANT ALL ON jose_carballo.* TO jose_carballo@'%';
+        
+CREATE DATABASE IF NOT EXISTS lautaro_salazar_de_moya;
+CREATE TABLE IF NOT EXISTS lautaro_salazar_de_moya.welcome (message VARCHAR(100));
+INSERT INTO lautaro_salazar_de_moya.welcome (message) VALUES ('Welcome to your database lautaro salazar de moya'); 
+CREATE USER lautaro_salazar_de_moya@'%' IDENTIFIED BY 'lautaro_salazar_de_moya';
+GRANT ALL ON lautaro_salazar_de_moya.* TO lautaro_salazar_de_moya@'%';
+        
+CREATE DATABASE IF NOT EXISTS liam_marin;
+CREATE TABLE IF NOT EXISTS liam_marin.welcome (message VARCHAR(100));
+INSERT INTO liam_marin.welcome (message) VALUES ('Welcome to your database liam marin'); 
+CREATE USER liam_marin@'%' IDENTIFIED BY 'liam_marin';
+GRANT ALL ON liam_marin.* TO liam_marin@'%';
+        
+CREATE DATABASE IF NOT EXISTS mario_martinez_galarza;
+CREATE TABLE IF NOT EXISTS mario_martinez_galarza.welcome (message VARCHAR(100));
+INSERT INTO mario_martinez_galarza.welcome (message) VALUES ('Welcome to your database mario martinez galarza'); 
+CREATE USER mario_martinez_galarza@'%' IDENTIFIED BY 'mario_martinez_galarza';
+GRANT ALL ON mario_martinez_galarza.* TO mario_martinez_galarza@'%';
+        
+CREATE DATABASE IF NOT EXISTS matias_eckhardt;
+CREATE TABLE IF NOT EXISTS matias_eckhardt.welcome (message VARCHAR(100));
+INSERT INTO matias_eckhardt.welcome (message) VALUES ('Welcome to your database matias eckhardt'); 
+CREATE USER matias_eckhardt@'%' IDENTIFIED BY 'matias_eckhardt';
+GRANT ALL ON matias_eckhardt.* TO matias_eckhardt@'%';
+        
+CREATE DATABASE IF NOT EXISTS maximiliano_vitti;
+CREATE TABLE IF NOT EXISTS maximiliano_vitti.welcome (message VARCHAR(100));
+INSERT INTO maximiliano_vitti.welcome (message) VALUES ('Welcome to your database maximiliano vitti'); 
+CREATE USER maximiliano_vitti@'%' IDENTIFIED BY 'maximiliano_vitti';
+GRANT ALL ON maximiliano_vitti.* TO maximiliano_vitti@'%';
+        
+CREATE DATABASE IF NOT EXISTS nicolas_maurizi;
+CREATE TABLE IF NOT EXISTS nicolas_maurizi.welcome (message VARCHAR(100));
+INSERT INTO nicolas_maurizi.welcome (message) VALUES ('Welcome to your database nicolas maurizi'); 
+CREATE USER nicolas_maurizi@'%' IDENTIFIED BY 'nicolas_maurizi';
+GRANT ALL ON nicolas_maurizi.* TO nicolas_maurizi@'%';
+        
+CREATE DATABASE IF NOT EXISTS pablo_defelitto;
+CREATE TABLE IF NOT EXISTS pablo_defelitto.welcome (message VARCHAR(100));
+INSERT INTO pablo_defelitto.welcome (message) VALUES ('Welcome to your database pablo defelitto'); 
+CREATE USER pablo_defelitto@'%' IDENTIFIED BY 'pablo_defelitto';
+GRANT ALL ON pablo_defelitto.* TO pablo_defelitto@'%';
+        
+CREATE DATABASE IF NOT EXISTS santiago_franchi;
+CREATE TABLE IF NOT EXISTS santiago_franchi.welcome (message VARCHAR(100));
+INSERT INTO santiago_franchi.welcome (message) VALUES ('Welcome to your database santiago franchi'); 
+CREATE USER santiago_franchi@'%' IDENTIFIED BY 'santiago_franchi';
+GRANT ALL ON santiago_franchi.* TO santiago_franchi@'%';
+        
+CREATE DATABASE IF NOT EXISTS veronica_ferreyra;
+CREATE TABLE IF NOT EXISTS veronica_ferreyra.welcome (message VARCHAR(100));
+INSERT INTO veronica_ferreyra.welcome (message) VALUES ('Welcome to your database veronica ferreyra'); 
+CREATE USER veronica_ferreyra@'%' IDENTIFIED BY 'veronica_ferreyra';
+GRANT ALL ON veronica_ferreyra.* TO veronica_ferreyra@'%';
+        
