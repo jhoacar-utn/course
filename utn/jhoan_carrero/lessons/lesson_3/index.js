5193f0717b82ba7587ef0588aa68e5f7b88158f4
jhoacar <jhoacar.utn@gmail.com>
2022-04-13 02:59:36 +0200

    added files for parameters with express

--- utn/jhoan_carrero/lessons/lesson_3/index.js
+++ utn/jhoan_carrero/lessons/lesson_3/index.js
@@ -3,31 +3,60 @@ const express = require("express");
 const app = express();
 
 const PORT = 4000;
-
 const NAME = "pepito";
 
-app.use('/lesson_1', express.static(__dirname+"/../lesson_1/index.html"));
+/*
+
+    GET -> los parametros son pasados por la URL
+
+        http://localhost:4000/?name=pepito&last_name=perez
+
+    POST -> request.body 
+        {
+            "email": "correo",
+            "passsword": "contrasena"
+        }
+    
+    /productos/limpieza
+    /productos/cosmeticos
+
+    /products/:type -> el trabajar con :type express colocara el nombre que se haya elegido en esa variable
+
+    /lesson_1/:student/:age
+
+    /lesson_1/pepito/23
+
+
+*/
+
+
+const restrictLesson_1 =  (request,response,next)=>{
+    
+    const student_name = request.params.student;
+
+    if(student_name===NAME)
+        next();
+    else
+        return response.send("Ud no es pepito y no puede ver la clase");
+}
+
+app.use('/lesson_1/:student',restrictLesson_1, express.static(__dirname+"/../lesson_1"));
 
 const handleRequest = (request,response,next)=>{
-    response.send("Welcome to my application");
+
+    return response.send("Welcome to my application");
 }
 
 
 const sendLesson_1 = (request,response,next)=>{
-    response.sendFile(__dirname+"/../lesson_1/index.html");
+    return response.sendFile(__dirname+"/../lesson_1/index.html");
 }
 
 
-const restrictLesson_1 =  (request,response,next)=>{
-    if(NAME=="pepito")
-        next();
-    else
-        response.send("Ud no es pepito y no puede ver la clase");
-}
 
 app.get("/",handleRequest);
 
-app.get("/lesson_1",restrictLesson_1,sendLesson_1);
+app.get("/lesson_1/:student",restrictLesson_1,sendLesson_1);
 
 
 
