5e33872ad7da26c77c46aeb1e6362df7200a14d9
jhoacar <jhoacar.utn@gmail.com>
2022-04-19 20:19:19 -0300

    added files for environment connection with mysql

--- utn/lessons/lesson_4/index.js
+++ utn/lessons/lesson_4/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require('dotenv').config()
 
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4001 ;
 
 
 const handleRequest = (req,res,next)=>{
