8da766aaff62e356ea5ade9c03621884810b4542
jhoacar <jhoacar.utn@gmail.com>
2022-04-19 21:51:15 -0300

    added message for frontend with the database

--- utn/lessons/lesson_4/config/mysql/connection.js
+++ utn/lessons/lesson_4/config/mysql/connection.js
@@ -9,11 +9,12 @@ module.exports = {
 
     executeQuery : function(query, callback){
 
-        connection.connect();
+        //connection.connect(); 
+        //Removido por problemas en la version de mysql que cierra y abre conexiones constantemente
 
         connection.query(query, callback);
 
-        connection.end();
+        //connection.end();
 
     }
 }
