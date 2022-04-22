472ccaef26ac57d3c3f829f8800d39dc643c870e
jhoacar <jhoacar.utn@gmail.com>
2022-04-22 20:47:18 -0300

    added end configuration app

--- utn/lessons/lesson_5/routes/web.js
+++ utn/lessons/lesson_5/routes/web.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const {getWelcomePage} = require("../controllers/webController");
 
 router.get("/",getWelcomePage);
-
-
 
 module.exports = router;
