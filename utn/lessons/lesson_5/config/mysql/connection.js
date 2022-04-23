21eb381b864e5451a5b4fa8005149b314c363b76
jhoacar <jhoacar.utn@gmail.com>
2022-04-22 22:18:56 -0300

    added fetch to register html

--- utn/lessons/lesson_5/config/mysql/connection.js
+++ utn/lessons/lesson_5/config/mysql/connection.js
@@ -1,21 +1,10 @@
-const MYSQL = require('mysql');
-
 const {mysql} = require("../config");
+const { Sequelize } = require('sequelize');
 
 
-const connection = MYSQL.createConnection(mysql);
-
-module.exports = {
-
-    executeQuery : function(query, callback){
-
-        //connection.connect(); 
-        //Removido por problemas en la version de mysql que cierra y abre conexiones constantemente
-
-        connection.query(query, callback);
-
-        //connection.end();
-
-    }
-}
-
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(mysql.database, mysql.user, mysql.password, {
+    host: mysql.host,
+    port: mysql.port,
+    dialect: 'mysql'
+  });
--- utn/lessons/lesson_5/index.js
+++ utn/lessons/lesson_5/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
 require('dotenv').config();
+const express = require("express");
+
 
 const routerApi = require("./routes/api");
 const routerWeb = require("./routes/web");
@@ -9,8 +10,13 @@ const {executeQuery} = require("./config/mysql/connection")
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use("/api",routerApi);
 app.use("/",routerWeb);
+
+
 
 const PORT = process.env.PORT || 4001 ;
 
