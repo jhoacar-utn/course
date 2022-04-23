87b08cc5a5b6537c255c0320802f5b7a93ec0944
jhoacar <jhoacar.utn@gmail.com>
2022-04-22 22:21:13 -0300

    removed executeQuery from index.js

--- utn/lessons/lesson_5/controllers/userController.js
+++ utn/lessons/lesson_5/controllers/userController.js
@@ -10,8 +10,6 @@ const postUser = (req,res,next)=>{
     
     const userData = req.body;
 
-
-
     console.log(userData)
 
     return res.json(userData);
--- utn/lessons/lesson_5/index.js
+++ utn/lessons/lesson_5/index.js
@@ -5,8 +5,6 @@ const express = require("express");
 const routerApi = require("./routes/api");
 const routerWeb = require("./routes/web");
 
-const {executeQuery} = require("./config/mysql/connection")
-
 
 const app = express();
 
@@ -24,12 +22,4 @@ const PORT = process.env.PORT || 4001 ;
 app.listen(PORT,()=>{
     
     console.log("Server on port ",PORT);
-
-    executeQuery("SELECT 1 + 1 AS solution",function(err, rows, fields) {
-        
-        if (err) throw err;
-
-        console.log("Connection enabled with mysql");
-        console.log('The solution is: ', rows[0].solution);
-      });
 });
