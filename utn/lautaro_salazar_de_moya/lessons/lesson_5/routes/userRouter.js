a17975eec020d7238b422d1aad0d47b8dc26322a
Lautaro <lautaro.salazardemoya@gmail.com>
2022-04-29 20:09:53 -0300

    Add lesson_7

--- utn/lautaro_salazar_de_moya/lessons/lesson_5/routes/userRouter.js
+++ utn/lautaro_salazar_de_moya/lessons/lesson_5/routes/userRouter.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const {getUsers, postUser} = require("../controllers/userController");
-const {postAuth} = require("../controllers/authController")
 
 router.get("/",getUsers);
 router.post("/",postUser);
-router.post("/",postAuth);
-
 
 module.exports = router;
---
+++ utn/lautaro_salazar_de_moya/lessons/lesson_7/models/authModel.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require("../config/mysql/connection");
+
+
+const auth = sequelize.define('User', {
+  // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password : {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+    tableName: 'users'
+});
+
+
+
+module.exports = auth;
+
