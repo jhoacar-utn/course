f8d4fc04b191636f03b281ed02943973e49bbd0a
jhoacar <carrerojhoan@gmail.com>
2022-04-30 19:31:44 -0300

    removed folder santiago franchi

--- utn/santiago_franchi/lessons/lesson_5/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {getUsers, postUser} = require("../controllers/userController")
-
-router.get("/",getUsers)
-router.post("/",postUser)
-
-module.exports = router
--- utn/santiago_franchi/lessons/lesson_7/controllers/apiController.js
+++
@@ -1,7 +0,0 @@
-
-const getDocApi = (req,res,next)=>{
-        return res.send("Doc Api")
-}
-
-module.exports.getDocApi = getDocApi
-
