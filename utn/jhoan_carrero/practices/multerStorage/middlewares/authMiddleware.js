37c4b7819f2d1be80b50ebeef5f30a25950663b2
jhoacar <carrerojhoan@gmail.com>
2022-04-30 16:39:03 -0300

    handle other driver using multer-google-storage

--- utn/jhoan_carrero/practices/multerStorage/middlewares/authMiddleware.js
+++ utn/jhoan_carrero/practices/multerStorage/middlewares/authMiddleware.js
@@ -1,28 +1,32 @@
 const { getPayloadData } = require("../helpers/handleJWT");
-const userModel = require("../models/userModel");
 
 const authMiddleware = async (req, res, next) => {
 
-    const token = req.session.userToken;
+    try {        
+        const token = req.session.userToken;
 
-    if (!token) {
-        res.status(403);
-        return res.json({ error: "Token is required" });
+        if (!token) {
+            res.status(403);
+            return res.json({ error: "Token is required" });
+        }
+
+        const userData = getPayloadData(token);
+
+        if (!userData) {
+            res.status(403);
+            return res.json({ error: "Token is invalid" });
+        }
+
+        // const email = userData.email;
+        // const user = await userModel.findOne({ where: { email } });
+
+        req.user = userData;
+
+        return next();
+
+    } catch (error) {
+        return res.redirect("/login");
     }
-
-    const userData = getPayloadData(token);
-
-    if (!userData) {
-        res.status(403);
-        return res.json({ error: "Token is invalid" });
-    }
-
-    // const email = userData.email;
-    // const user = await userModel.findOne({ where: { email } });
-
-    req.user = userData;
-
-    return next();
 
 }
 
