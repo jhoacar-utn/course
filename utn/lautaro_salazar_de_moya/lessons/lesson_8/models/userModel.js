0053a479779edc53e3f985866c02a2ad5d27c408
Lautaro <lautaro.salazardemoya@gmail.com>
2022-05-03 19:47:52 -0300

    the second  begining of lesson 8

---
+++ utn/lautaro_salazar_de_moya/lessons/lesson_8/models/userModel.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require("../config/mysql/connection");
+
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password : {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+    tableName: 'users'
+});
+
+
+
+module.exports = User;
+
