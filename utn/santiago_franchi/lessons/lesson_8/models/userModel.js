ffdb4f6ce2843d528b543dd4c445523a33352e37
tagonio <73574811+tagonio@users.noreply.github.com>
2022-05-06 22:17:57 -0300

    lesson 8 and lesson 9

---
+++ utn/santiago_franchi/lessons/lesson_8/models/userModel.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require("../config/mysql/connection");
+
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password : {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  avatar : {
+    type: DataTypes.STRING,
+  },
+
+}, {
+    tableName: 'users'
+});
+
+
+
+module.exports = User;
+
---
+++ utn/santiago_franchi/lessons/lesson_9/config/api/init.js
@@ -0,0 +1,19 @@
+const isConnectedApi = require('./connection')
+
+const initDatabase = async () => {
+
+    try {
+        const response = await isConnectedApi();
+        
+        if(response.status === 200)
+            console.log("Connection with api enabled");
+        else
+            console.log("Connection not enabled")
+            
+    } catch (error) {
+        console.log("Error connection", error);
+    }
+}
+
+
+module.exports = initDatabase;
