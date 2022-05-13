c00f0d81902b7db3e36f294a8d979f47bf500af2
tinxnit <martinxpereyra@gmail.com>
2022-05-13 17:26:46 -0300

    git

---
+++ utn/martin_pereyra/lessons/lesson_4/config/mysql/connection.js
@@ -0,0 +1,20 @@
+
+const MYSQL = require('mysql');
+
+const {mysql} = require("../config");
+
+
+const connection = MYSQL.createConnection(mysql);
+
+module.exports = {
+
+    executeQuery : function(query, callback){
+
+        connection.connect();
+
+        connection.query(query, callback);
+
+        connection.end();
+
+    }
+}
---
+++ utn/martin_pereyra/lessons/lesson_4/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require('dotenv').config();
+
+const {executeQuery} = require("./config/mysql/connection")
+
+const app = express();
+
+const PORT = process.env.PORT || 4001 ;
+
+
+const handleRequest = (req,res,next)=>{
+    return res.send("Welcome to my app");
+}
+
+app.get("/",handleRequest);
+
+
+app.listen(PORT,()=>{
+    
+    console.log("Server on port ",PORT);
+
+    executeQuery("SELECT 1 + 1 AS solution",function(err, rows, fields) {
+        
+        if (err) throw err;
+
+        console.log("Connection enabled with mysql");
+        console.log('The solution is: ', rows[0].solution);
+      });
+});
