ccdb918d6105f947f814a3e08e1531b66c2da3d9
jhoacar <jhoacar.utn@gmail.com>
2022-05-17 22:09:57 -0300

    added serve angular compiled

--- utn/lessons/lesson_12/build/index.js
+++ utn/lessons/lesson_12/build/index.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 4000;
+const buildClient = "client/dist/client";
+const pathStatic = path_1.default.resolve(`${__dirname}/../${buildClient}`);
+app.use("/", express_1.default.static(pathStatic));
 app.listen(port, () => {
     console.log("App is up");
 });
--- utn/lessons/lesson_12/src/index.ts
+++ utn/lessons/lesson_12/src/index.ts
@@ -1,9 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
+import path from "path";
 
 const app = express();
 
 const port = 4000;
 
+const buildClient = "client/dist/client";
+const pathStatic = path.resolve(`${__dirname}/../${buildClient}`);
+
+app.use("/",express.static(pathStatic));
 
 
 app.listen(port,()=>{
