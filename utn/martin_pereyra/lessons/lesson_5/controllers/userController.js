064c2dbab0206fb24fbd025c7f93495205147820
tinxnit <martinxpereyra@gmail.com>
2022-05-18 11:36:01 -0300

    5 traemos datos cliente

--- utn/martin_pereyra/lessons/lesson_5/controllers/userController.js
+++ utn/martin_pereyra/lessons/lesson_5/controllers/userController.js
@@ -4,7 +4,10 @@ const getUsers = (req,res,next)=>{
 }
 
 const postUser = (req,res,next)=>{
-    return res.send("User added");
+    //traemos la info del usuario que viaja desde la req del body
+    const userData = req.body;
+    //como lo que estoy trajendo es un objeto (de datos del cliente) uso Json
+    return res.json(userData); //express no lee por defecto no lee por defecto el body. Hay que especificarlo en index.js  (hora 2 clase 5)
 }
 
 module.exports.getUsers = getUsers;
