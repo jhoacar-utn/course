09d8576b5b0cbf94f1481defab675945e5f41893
matieckhardt <matias.eckhardt@gmail.com>
2022-05-20 20:21:31 -0300

    added lessons 8 to 13

---
+++ utn/matias_eckhardt/lessons/lesson_12/build/namespace/letterValidation.js
@@ -0,0 +1,11 @@
+"use strict";
+var Validation;
+(function (Validation) {
+    const lettersRegexp = /^[A-Za-z]+$/;
+    class LettersOnlyValidator {
+        isAcceptable(s) {
+            return lettersRegexp.test(s);
+        }
+    }
+    Validation.LettersOnlyValidator = LettersOnlyValidator;
+})(Validation || (Validation = {}));
