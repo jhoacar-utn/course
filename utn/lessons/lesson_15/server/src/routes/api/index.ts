debe661ac5530046ef88b00372587d8dd04551db
jhoacar <jhoacar.utn@gmail.com>
2022-06-03 19:39:09 -0300

    addded backend test

--- utn/lessons/lesson_15/server/src/routes/api/index.ts
+++ utn/lessons/lesson_15/server/src/routes/api/index.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from "express";
+import authRouter from './auth';
 
 const router = Router();
 
-router.get("/",(req:Request, res:Response)=>{
+router.get("/", (req: Request, res: Response) => {
     return res.send("Estoy en la api");
 })
+
+router.use("/auth", authRouter);
 
 export default router;
