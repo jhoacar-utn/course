6b14c9f5663a8668b7bb039b442d50d0ce692e99
jhoacar <jhoacar.utn@gmail.com>
2022-06-03 20:05:13 -0300

    added children in card using layout

--- utn/lessons/lesson_15/client/src/components/Footer/index.js
+++ utn/lessons/lesson_15/client/src/components/Footer/index.js
@@ -1,8 +1,19 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import './index.css';
 
 
 function Footer() {
-    return <div className='nav-link'>Hecho por Jhoan</div>
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{
+                minHeight: "5vh",
+                textAlign: "center"
+            }}>
+                <h4>Hecho por Jhoan</h4>
+            </AppBar>
+        </Box>
+    );
 }
 
 export default Footer;
--- utn/lessons/lesson_15/client/src/components/Layout.js
+++ utn/lessons/lesson_15/client/src/components/Layout.js
@@ -1,12 +1,20 @@
+import { Card } from "@mui/material";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
-function Layout({children}) {
+function Layout({ children }) {
 
     return (
         <>
             <NavBar></NavBar>
-            {children}
+            <Card sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                minHeight:"80vh"
+            }}>
+                {children}
+            </Card>
             <Footer></Footer>
         </>
     );
