ebf52315c9e1f3ae538fbff4f35c43872ad4bbcf
jhoacar <jhoacar.utn@gmail.com>
2022-06-03 21:14:03 -0300

    added styles in login form

--- utn/lessons/lesson_15/client/src/pages/Login.js
+++ utn/lessons/lesson_15/client/src/pages/Login.js
@@ -1,26 +1,72 @@
-import { Button, Card, CardContent, Input } from "@mui/material";
+import { Button, Card, CardContent, FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 import Layout from "../components/Layout";
 
 
 function Login() {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+
+        console.log("Enviar formulario")
+    }
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
     return (
         <Layout>
-            <Card sx={{ maxWidth: 275 }} >
-                <CardContent>
-                    <Box component="form">
-                        <div>
-                            <Input type="email"></Input>
-                        </div>
-                        <div>
-                            <Input type="password"></Input>
-                        </div>
-                        <div>
-                            <Button>Log In</Button>
-                        </div>
-                    </Box>
-                </CardContent>
+            <Card sx={{
+                minHeight: 400,
+                minWidth: 400,
+                bgcolor: 'info.main'
+            }} >
+                <form onSubmit={handleSubmit}>
+                    <CardContent sx={{
+                        minHeight: 400,
+                        minWidth: 400,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: "50px"
+                    }}>
+                        <Box>
+                            <FormControl>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" aria-describedby="email-helper" />
+                                <FormHelperText id="email-helper">We'll never share your email.</FormHelperText>
+                            </FormControl>
+                        </Box>
+                        <Box>
+                            <FormControl>
+                                <InputLabel htmlFor="email">Password</InputLabel>
+                                <Input id="password" name="password" aria-describedby="password-helper" />
+                                <FormHelperText id="password-helper">Please type your password.</FormHelperText>
+                            </FormControl>
+                        </Box>
+                        <Box sx={{
+                            marginTop: "10px",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            display: "flex"
+                        }}>
+                            <Button sx={{
+                                color: 'text.primary'
+                            }}>
+                                <Input type="submit" value="Log In" />
+                            </Button>
+                        </Box>
+                    </CardContent>
+                </form>
             </Card>
         </Layout>
     )
