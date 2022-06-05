3210747e885f1230876ea450147e23f677821949
pilo84 <pablidefelitto@gmail.com>
2022-06-05 20:48:04 -0300

    added database connection

---
+++ utn/pablo_defelitto/project/server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+//const cors = require("cors");
+const {executeQuery} = require("./config/mysql/connection");
+const app = express();
+
+//app.use(cors());
+const PORT =process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+
+    console.log(`server on in port ${PORT}`);
+
+    executeQuery('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+        if (err) throw err;
+        console.log('The solution is: ', rows[0].solution);
+        console.log("create connection");
+      });
+});
+
+
