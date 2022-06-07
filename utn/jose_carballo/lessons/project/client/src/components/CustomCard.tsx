17f48596967f8cc28171970ac2907a4b0310ffbf
Jose Carballo <jose.carballo.so@softtek.com>
2022-06-06 21:04:40 -0300

    cambio en el diseño de la card

--- utn/jose_carballo/lessons/project/client/src/components/CustomCard.tsx
+++ utn/jose_carballo/lessons/project/client/src/components/CustomCard.tsx
@@ -2,26 +2,28 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Box } from '@mui/material';
 
 export const CustomCard = (pokemon: any) =>{
   const {pokemon: pokemonData} = pokemon;
   const {name, sprites} = pokemonData
   return (
-    <Card sx={{margin:2, maxWidth: 120 }}>
+    <Card sx={{margin:2, minWidth: 200, display: 'flex' }}>
+       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="50"
-          image={sprites?.front_default}
-          alt="green iguana"
-        />
         <CardContent>
           <Typography gutterBottom>
             {name}
           </Typography>
         </CardContent>
       </CardActionArea>
+      </Box>
+      <CardMedia
+          component="img"
+          height="80"
+          image={sprites?.front_default}
+          alt={name}
+        />
     </Card>
   );
 }
