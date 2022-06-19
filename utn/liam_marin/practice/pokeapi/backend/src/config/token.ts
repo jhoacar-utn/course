94e175261ea08359f214084acdd9f6af4648c2fc
Liam Marin <liam.marin03926@protonmail.com>
2022-06-19 11:45:36 -0300

    fix typo in env var name

--- utn/liam_marin/practice/pokeapi/backend/src/config/token.ts
+++ utn/liam_marin/practice/pokeapi/backend/src/config/token.ts
@@ -1,5 +1,5 @@
 const config = (() => {
-  const secret = process.env.TOKEN_SECREKEY;
+  const secret = process.env.TOKEN_SECRETKEY;
 
   if (!secret) {
     throw new Error("TOKEN_SECRETKEY is unset");
