b2cf00a904be706adf1a0cbe901b06de140092eb
Liam Marin <liam.marin03926@protonmail.com>
2022-06-21 03:34:36 -0300

    add checkToken middleware

---
+++ utn/liam_marin/practice/pokeapi/backend/src/middleware/checkToken.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from "express";
+
+import jsonwebtoken from "jsonwebtoken";
+import config from "../config/token";
+
+function checkToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.header("Authorization");
+  if (!token) {
+    res.status(401).json({ message: "Unauthorized." });
+    return;
+  }
+
+  jsonwebtoken.verify(
+    token,
+    config.secret,
+    { algorithms: ["HS256"] },
+    (err, decoded) => {
+      if (err || decoded === undefined || typeof decoded === "string") {
+        res.status(400).json({ message: "Invalid Authorization header." });
+      } else {
+        res.locals.token = decoded;
+        next();
+      }
+    }
+  );
+}
+
+export default checkToken;
